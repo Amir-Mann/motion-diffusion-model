6a17ab60ce510d2a8ba5aac7e3768e752420ba16
Amir Mann <amir.mann@newton.csf.technion.ac.il>
2024-12-15 20:01:21 +0200

    Download changes to use local gdown installstion. and using writergif = matplotlib.animation.PillowWriter(fps=30) to write gif for visualization.

--- prepare/download_t2m_evaluators.sh
+++ prepare/download_t2m_evaluators.sh
@@ -1,6 +1,6 @@
 echo -e "Downloading T2M evaluators"
-gdown --fuzzy https://drive.google.com/file/d/1DSaKqWX2HlwBtVH5l7DdW96jeYUIXsOP/view
-gdown --fuzzy https://drive.google.com/file/d/1tX79xk0fflp07EZ660Xz1RAFE33iEyJR/view
+/home/amir.mann/.local/bin/gdown --fuzzy https://drive.google.com/file/d/1DSaKqWX2HlwBtVH5l7DdW96jeYUIXsOP/view
+/home/amir.mann/.local/bin/gdown --fuzzy https://drive.google.com/file/d/1tX79xk0fflp07EZ660Xz1RAFE33iEyJR/view
 rm -rf t2m
 rm -rf kit
 
